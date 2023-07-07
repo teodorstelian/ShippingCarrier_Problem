1bb056ab69c834a8116906ac2e0136f0fc755a71
Teodor-Stelian Baltaretu <teodorstelian98@yahoo.com>
2023-07-07 12:32:32 +0300

    Changed unittests

--- tests/test_shipping_classes.py
+++ tests/test_shipping_classes.py
@@ -11,7 +11,7 @@ class TestShippingClasses(unittest.TestCase):
         shipping_option.shipping_method = "standard"
         shipping_cost = shipping_option.get_shipping_cost()
         expected_cost = 1.25
-        self.assertEqual(shipping_cost, expected_cost)
+        assert shipping_cost == expected_cost
 
     def test_get_shipping_cost_express(self):
         shop = Shop("Shop 2", 1.8)
@@ -20,15 +20,15 @@ class TestShippingClasses(unittest.TestCase):
         shipping_option.shipping_method = "express"
         shipping_cost = shipping_option.get_shipping_cost()
         expected_cost = 1.8
-        self.assertEqual(shipping_cost, expected_cost)
+        assert shipping_cost == expected_cost
 
     def test_connect_to_shop(self):
         shop = Shop("Shop 3", 1.2)
         carrier = Carrier("Carrier 3", "Country 3")
         shipping_option = carrier.connect_to_shop(shop)
-        self.assertEqual(shipping_option.shop, shop)
-        self.assertEqual(shipping_option.name, carrier.name)
-        self.assertEqual(shipping_option.country, carrier.country)
+        assert shop == shipping_option.shop
+        assert carrier.name == shipping_option.name
+        assert carrier.country == shipping_option.country
 
     def test_shipping_option_attributes(self):
         shop = Shop("Shop 4", 2.0)
@@ -36,16 +36,16 @@ class TestShippingClasses(unittest.TestCase):
         shipping_option = carrier.connect_to_shop(shop)
         shipping_option.shipping_method = "standard"
         shipping_option.shipping_cost = 1.0
-        self.assertEqual(shipping_option.shop, shop)
-        self.assertEqual(shipping_option.name, carrier.name)
-        self.assertEqual(shipping_option.country, carrier.country)
-        self.assertEqual(shipping_option.shipping_method, "standard")
-        self.assertEqual(shipping_option.shipping_cost, 1.0)
+        assert shipping_option.shop == shop
+        assert shipping_option.name == carrier.name
+        assert shipping_option.country == carrier.country
+        assert shipping_option.shipping_method == "standard"
+        assert shipping_option.shipping_cost == 1.0
 
     def test_shop_attributes(self):
         shop = Shop("Shop 5", 1.5)
-        self.assertEqual(shop.name, "Shop 5")
-        self.assertEqual(shop.package_weight, 1.5)
+        assert shop.name == "Shop 5"
+        assert shop.package_weight == 1.5
 
 
 if __name__ == "__main__":
